a4d12e25206ea97ac74bdc689d920bae197dd41e
awais1515 <awais.ahmadfcc@gmail.com>
2022-04-03 12:23:19 +0500

    shape and style added

--- app/src/main/java/com/myappcompany/rob/exampleapp/MainActivity.java
+++ app/src/main/java/com/myappcompany/rob/exampleapp/MainActivity.java
@@ -7,15 +7,10 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction(View view) {
-
-        Log.i("Info", "Button pressed!");
-
-    }   
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
 }
